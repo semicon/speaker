3fd34f544a1333f6192552ff1666242caa2e2261
semicon <30399464+semicon@users.noreply.github.com>
2019-03-12 18:03:06 +0700

    speaker
    speaker

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1552388116,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/pic-sheet0.png",
+		"images/pic-sheet1.png",
+		"images/bg-sheet0.png",
+		"images/button-sheet0.png",
+		"images/button-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
