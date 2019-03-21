42ca8ae8ff1ba2b8731c2e81e9bea9460b08634d
semicon <30399464+semicon@users.noreply.github.com>
2019-03-21 21:31:49 +0700

    speaker
    speaker

--- offline.js
+++ offline.js
@@ -1,5 +1,5 @@
 ﻿{
-	"version": 1552551380,
+	"version": 1553170512,
 	"fileList": [
 		"data.js",
 		"c2runtime.js",
@@ -10,6 +10,8 @@
 		"images/bg-sheet0.png",
 		"images/button-sheet0.png",
 		"images/button-sheet1.png",
+		"images/sprite-sheet0.png",
+		"images/sprite2-sheet0.png",
 		"icon-16.png",
 		"icon-32.png",
 		"icon-114.png",
